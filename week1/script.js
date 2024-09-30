53d1f18cdb701572b62a7aded29720eadd43a49a
Yan Zhao <36881634-yanzhao8@users.noreply.replit.com>
2024-09-30 00:42:17 +0000

    deleted variables

--- week1/script.js
+++ week1/script.js
@@ -2,5 +2,3 @@ let firstName = "Yan";
 let lastName = "Zhao";
 let yearsOfStudy = "1";
 let goal = "become a Web front-end developer";
-let currentYear = 2024;
-let birthYear = currentYear - yearsOfStudy;
